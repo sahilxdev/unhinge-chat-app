4be91577d2cc712490596dcb170c918190e22ebd
Sahil Meena <sahilmeena2031@gmail.com>
2024-03-22 13:05:54 +0530

    created layout, more components,loading skeleton page,  protected routes for users without login , added input validators

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import React from "react";
+import { orange } from "../../constants/color";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <Box sx={{ flexGrow: 1 }} height={"4rem"}>
+        <AppBar
+          position="static"
+          sx={{
+            bgcolor: orange,
+          }}
+        >
 
-export default Header
+          <Toolbar>
+
+            <Typography sx={{
+              display: { xs: "none", sm:"block" }
+            }} >
+              Unhinged
+            </Typography>
+
+
+          </Toolbar>
+
+        </AppBar>
+      </Box>
+    </>
+  );
+};
+
+export default Header;
